5ea797d19bebeb1b2999aa8832ef063cf2758321
Vinay <vinay061@gmail.com>
2021-04-07 21:02:03 +0200

    Routes, Pages, NavLinks updated

---
+++ src/components/common/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    const activeStyle = { color : "#F15B2A"};
+    
+    return (
+        <nav>
+            <NavLink to='/' activeStyle={activeStyle} exact>Home</NavLink>{" | "}
+            <NavLink to='/about' activeStyle={activeStyle}>About</NavLink>{" | "}
+            <NavLink to='/course' activeStyle={activeStyle}>Course</NavLink>{" | "}
+            
+        </nav>
+    )
+}
+
+export default Header;
---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CoursePage extends React.Component {
+    render(){
+        return (
+            <h1>Courses</h1>
+        );
+    }
+}
+
+export default CoursePage;
